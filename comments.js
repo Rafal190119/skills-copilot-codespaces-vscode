b36788527cfebad2c132d2d7098b1977219bab0f
Rafal <91943655+Rafal190119@users.noreply.github.com>
2025-02-17 15:51:32 +0000

    Copilot third d commit

--- comments.js
+++ comments.js
@@ -1,10 +1,11 @@
 // create web server
-const http = require('http');
-const server = http.createServer((req, res) => {
-    if (req.url === '/comments') {
-        res.write('This is a comments page');
-        res.end();
-    }
-});
-server.listen(3000);
-console.log('Listening on port 3000...');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+
+// create server
+http.createServer(function(req, res) {
+  var pathname = url.parse(req.url).pathname;
+  console.log('Request for ' + pathname + ' received.');
+  //
